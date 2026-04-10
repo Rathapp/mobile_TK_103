3f97d88aa8bf9accdb1d218f5e91e0d87a7b923c
Pov Ratha <125867348+Rathapp@users.noreply.github.com>
2026-04-10 20:59:48 +0700

    update story of facebook

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,34 +14,69 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: _buildScaffold(),
-    );
+    return MaterialApp(home: _buildScaffold());
   }
 
   Widget _buildScaffold() {
-    return Scaffold(
-
-      body: _buildBody(),
-    );
+    return Scaffold(body: _buildBody());
   }
 
-  Widget _buildStory(){
-    return Padding(
-      padding: EdgeInsetsGeometry.all(3),
-      child: Container(
-        width: 80,
-        height: 120,
-        decoration: BoxDecoration(
-          color: Colors.grey,
-          borderRadius: BorderRadius.circular(10)
-        ),
+  Widget _buildStory() {
+    return Container(
+      margin: EdgeInsetsGeometry.only(left: 6),
+      clipBehavior: Clip.hardEdge,
+
+      width: 125,
+      height: 200,
+      decoration: BoxDecoration(
+        color: Colors.grey,
+        borderRadius: BorderRadius.circular(13),
+        border: Border.all(color: Colors.white70, width: 1)
+      ),
+      child: Stack(
+        children: [
+          Image.asset(
+            "assets/images/sss.jpg",
+            width: 125,
+            height: 200,
+            fit: BoxFit.cover,
+          ),
+          Positioned(
+            left: 8,
+            top: 8,
+            child: Container(
+              padding: EdgeInsetsGeometry.all(2),
+              width: 35,
+              height: 35,
+              decoration: BoxDecoration(
+                color: Colors.transparent,
+                borderRadius: BorderRadius.circular(35),
+                border: Border.all(
+                  color: Colors.indigoAccent,
+                  width: 2,
+                ),
+              ),
+              alignment: Alignment.center,
+              child: CircleAvatar(
+                radius: 15,
+                backgroundImage: AssetImage(
+                  "assets/images/ddd.jpeg"
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+              left: 8,
+              bottom: 5,
+              child: Text("Pov Ratha",style: TextStyle(color: Colors.white,fontSize: 15,fontWeight: FontWeight(700)),))
+        ],
       ),
     );
   }
 
   Widget _buildBody() {
     return SingleChildScrollView(
+      physics: ClampingScrollPhysics(),
       child: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -56,32 +92,37 @@ class MyApp extends StatelessWidget {
                 fontWeight: FontWeight.bold,
               ),
             ),
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(children: [
-                _buildStory(),
-                _buildStory(),
-                _buildStory(),
-                _buildStory(),
-                _buildStory(),
-                _buildStory(),
-                _buildStory(),
-                _buildStory(),
-                _buildStory(),
-                _buildStory(),
-                _buildStory(),
-                _buildStory(),
-              ],),
+            Padding(
+              padding: EdgeInsetsGeometry.only(left: 5, right: 5),
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    _buildStory(),
+                    _buildStory(),
+                    _buildStory(),
+                    _buildStory(),
+                    _buildStory(),
+                    _buildStory(),
+                    _buildStory(),
+                    _buildStory(),
+                    _buildStory(),
+                    _buildStory(),
+                    _buildStory(),
+                    _buildStory(),
+                  ],
+                ),
+              ),
             ),
             _buildImage(),
             Padding(
               padding: EdgeInsetsGeometry.all(30),
-              child: Text("After a month and a half of spiraling conflict in the Middle East, the United States and Iran agreed to a two-week ceasefire on Tuesday – less than two hours before US President Donald Trump s deadline, after which he had promised to wipe out a whole civilizatio",
-              textAlign: TextAlign.justify,
-              style: TextStyle(
-                fontSize: 20,
-              )),
-            )
+              child: Text(
+                "After a month and a half of spiraling conflict in the Middle East, the United States and Iran agreed to a two-week ceasefire on Tuesday – less than two hours before US President Donald Trump s deadline, after which he had promised to wipe out a whole civilizatio",
+                textAlign: TextAlign.justify,
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
           ],
         ),
         // Icon(CupertinoIcons.heart,color: Colors.amber,size: 56)
@@ -94,74 +135,72 @@ class MyApp extends StatelessWidget {
 
   Widget _buildRow() {
     return Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              "Hello",
-              style: TextStyle(
-                fontSize: 25,
-                color: Colors.green,
-                fontWeight: FontWeight(600),
-              ),
-            ),
-            Stack(
-              clipBehavior: Clip.none,
-              children: [
-                _buildIcon(),
-                _buildPosition()
-              ]
-            ),
-          ],
-        );
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          "Hello",
+          style: TextStyle(
+            fontSize: 25,
+            color: Colors.green,
+            fontWeight: FontWeight(600),
+          ),
+        ),
+        Stack(
+          clipBehavior: Clip.none,
+          children: [_buildIcon(), _buildPosition()],
+        ),
+      ],
+    );
   }
 
   Positioned _buildPosition() {
     return Positioned(
-                top: -8,
-                  right: -8,
-                  child: Container(
-                      width: 25,
-                      height: 25,
-                      alignment: Alignment.center,
-                      decoration: BoxDecoration(
-                        color: Colors.red,
-                        borderRadius: BorderRadius.circular(25)
-                      ),
-                      child: Text("3",style: TextStyle(color: Colors.white,fontSize: 18,fontWeight: FontWeight(700)),))
-              );
+      top: -8,
+      right: -8,
+      child: Container(
+        width: 25,
+        height: 25,
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          color: Colors.red,
+          borderRadius: BorderRadius.circular(25),
+        ),
+        child: Text(
+          "3",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 18,
+            fontWeight: FontWeight(700),
+          ),
+        ),
+      ),
+    );
   }
 
   Container _buildIcon() {
     return Container(
-                height: 56,
-                width: 56,
-                alignment: Alignment.center,
-                decoration: BoxDecoration(
-                  // border: Border.all(color: Colors.blueAccent, width: 2),
-                    color: Colors.grey,
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black26,
-                        blurRadius: 8,
-                        offset: Offset(2, 2),
-                      )
-                    ],
+      height: 56,
+      width: 56,
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+        // border: Border.all(color: Colors.blueAccent, width: 2),
+        color: Colors.grey,
+        boxShadow: [
+          BoxShadow(color: Colors.black26, blurRadius: 8, offset: Offset(2, 2)),
+        ],
 
-                    borderRadius:BorderRadius.circular(56)),
-                child: Icon(
-                  CupertinoIcons.heart,
-                  color: Colors.amber,
-                  size: 25,
-                ),
-              );
+        borderRadius: BorderRadius.circular(56),
+      ),
+      child: Icon(CupertinoIcons.heart, color: Colors.amber, size: 25),
+    );
   }
 
   Widget _buildImage() {
     return Image.asset(
-          "assets/images/ddd.jpeg",
-          width: 300,
-          height: 500,
-          fit: BoxFit.cover,
-        );
+      "assets/images/ddd.jpeg",
+      width: 300,
+      height: 500,
+      fit: BoxFit.cover,
+    );
   }
 }
